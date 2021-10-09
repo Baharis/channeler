a9c8a04b2fc0912d2c2c6b31559365f25f19c9ad
Daniel Tchoń <dantch@onet.eu>
2021-10-09 23:15:54 +0200

    Modified example, added splitting capability.

--- README.md
+++ README.md
@@ -1,4 +1,12 @@
 # channeler
-A minimal python3 package for splitting and merging colour channels of png pictures.
+A minimal python3 package designed for quick splitting and merging
+colour channels of png pictures. 
 
-To test, run the file channeler without further modification. A colourful image 'merged.png' should be created out of three example layers. The image is courtesy of Jon Sullivan from PDPhoto.org.
+To test the code, run the file channeler.py without further modifications.
+Image 'example.png' should be split into four layers containing
+information about red, green, blue and transparency.
+Afterwards image 'merged.png' should be created from these layers
+arranged in different configuration. Adapt the code to your preferences.
+
+This small tool is made by Daniel Tchoń, dtchon at chem.uw.edu.pl.
+The image is courtesy of Jon Sullivan from PDPhoto.org.
--- channeler.py
+++ channeler.py
@@ -22,12 +22,12 @@ def build_path(*path_elements):
     return path.abspath(path.expanduser(path.join(*path_elements)))
 
 
-def merge(r, g, b, a='', path='', out='merged.png'):
+def merge(r, g, b, a='', directory='', name='merged.png'):
     """
     Merge three (or four) png images into one using their r/g/b(/a) channels.
 
-    :param path: Directory where input and output figures are to be found.
-    :type path: str
+    :param directory: Directory where input and output figures are to be found.
+    :type directory: str
     :param r: file name of .png file holding the red channel for desired merge
     :type r: str
     :param g: file name of .png file holding the green channel for desired merge
@@ -36,22 +36,41 @@ def merge(r, g, b, a='', path='', out='merged.png'):
     :type b: str
     :param a: file name of .png file holding the alpha channel, if necessary
     :type a: str
-    :param out: fine name of .png file created after the merge
-    :type out: str
-    :return: None
-    :rtype: None
+    :param name: fine name of .png file created after the merge
+    :type name: str
     """
-    o = build_path(path, out)
-    r = Image.open(build_path(path, r)).convert('RGBA').getchannel(0)
-    g = Image.open(build_path(path, g)).convert('RGBA').getchannel(1)
-    b = Image.open(build_path(path, b)).convert('RGBA').getchannel(2)
+    o = build_path(directory, name)
+    r = Image.open(build_path(directory, r)).convert('RGBA').getchannel(0)
+    g = Image.open(build_path(directory, g)).convert('RGBA').getchannel(1)
+    b = Image.open(build_path(directory, b)).convert('RGBA').getchannel(2)
     if a is not '':
-        a = Image.open(build_path(path, a)).convert('RGBA').getchannel(3)
+        a = Image.open(build_path(directory, a)).convert('RGBA').getchannel(3)
         Image.merge('RGBA', (r, g, b, a)).save(o, 'PNG')
     else:
         Image.merge('RGB', (r, g, b)).save(o, 'PNG')
 
 
+def split(directory='', name=''):
+    """
+    Split png file into four based on their r/g/b/a channels.
+
+    :param directory: Directory where input and output figures are to be found.
+    :type directory: str
+    :param name: fine name of .png file created after the merge
+    :type name: str
+    """
+    d = directory
+    r_path = build_path(d, path.splitext(name)[0] + '_r.png')
+    g_path = build_path(d, path.splitext(name)[0] + '_g.png')
+    b_path = build_path(d, path.splitext(name)[0] + '_b.png')
+    a_path = build_path(d, path.splitext(name)[0] + '_a.png')
+    Image.open(build_path(d, name)).convert('RGBA').getchannel(0).save(r_path)
+    Image.open(build_path(d, name)).convert('RGBA').getchannel(1).save(g_path)
+    Image.open(build_path(d, name)).convert('RGBA').getchannel(2).save(b_path)
+    Image.open(build_path(d, name)).convert('RGBA').getchannel(3).save(a_path)
+
+
 if __name__ == '__main__':
     n = 'example{}.png'
-    merge(n.format('_r'), n.format('_g'), n.format('_b'))
+    split(name='example.png')
+    merge(n.format('_g'), n.format('_b'), n.format('_r'), a=n.format('_a'))
